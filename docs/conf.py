cba2b90f86a97020730edf18e0e8ad06c26c066a
David Ghedini <david@davidghedini.com>
2022-05-29 15:25:48 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,8 +12,8 @@ from sphinx.locale import _
 from sphinx_rtd_theme import __version__
 
 
-project = u'AcuGIS PostGIS Builder'
-copyright = u'2020, Cited, Inc.'
+project = u'AcuGIS Scout'
+copyright = u'2022, Cited, Inc.'
 slug = re.sub(r'\W+', '-', project.lower())
 version = __version__
 release = __version__
